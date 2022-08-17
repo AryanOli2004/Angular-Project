5c795186f5802b35e8cd5ae339390237d06c6bc0
AryanOli2004 <oliaryan2004@gmail.com>
2022-08-17 13:24:55 +0530

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,19 +8,19 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Bright Memory: Infinite',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'Bright Memory: Infinite  </b> is set in a sprawling, futuristic metropolis In the year 2036, a strange phenomenon for which scientists can find no explanation has occurred in the skies around the world.The Supernatural Science Research Organization (SRO) has sent agents out to various regions to investigate this phenomenon. It is soon discovered that these strange occurrences are connected to an archaic mystery-an as-of-yet unknown history of two worlds, about to come to light- Bright Memory: Infinite combines the FPS and action genres to deliver a high-octane experience. Mix and match available skills and abilities to unleash magnificent combos on your enemies. Your trusted sword allows you to slash through crowds of enemies and even repel their gunfire. The guns you come across in-game can be customized with a variety of ammunitions. Choose between incendiary bombs, sticky grenades, homing missiles, and more to suit your situation.'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'Yakuza 6: The Song Of Life',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Days Gone',
     price: 299,
     description: ''
   }
